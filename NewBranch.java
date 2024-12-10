e5344c39eb0affe6cae5fa96e3e1b10ac4930ff3
Soumik Jana <soumikjana914@gmail.com>
2024-12-10 08:52:10 +0530

    Updated NewBranch.java

--- NewBranch.java
+++ NewBranch.java
@@ -1,6 +1,6 @@
 public class NewBranch {
     public static void main(String[] args){
-        System.out.println("I am creating a new branch");
+        System.out.println("I am creating new branch");
         
     }
     
